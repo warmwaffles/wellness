72f9b8e98f1ddf9b3fa40352495b871daf6547b3
Matthew Johnston <warmwaffles@gmail.com>
2014-02-05 12:29:33 -0600

    Removed extra services require

--- lib/wellness/services.rb
+++ lib/wellness/services.rb
@@ -4,6 +4,3 @@ module Wellness
 end
 
 require 'wellness/services/base'
-require 'wellness/services/postgres_service'
-require 'wellness/services/redis_service'
-require 'wellness/services/sidekiq_service'
